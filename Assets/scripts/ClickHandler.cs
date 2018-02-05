8e9113eb4974d65358df7baf51a780d5f1de931d
binibabu22 <32741179+binibabu22@users.noreply.github.com>
2018-02-04 21:11:01 -0500

    Initial commit

---
+++ Assets/scripts/ClickHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour {
+
+    public GameObject transaparentshirt; 
+    public Sprite firstSprite; 
+    private SpriteRenderer spriterender;
+  
+    void Start()
+    {
+        spriterender = transaparentshirt.GetComponent<SpriteRenderer>();
+
+    }
+  
+    void OnMouseDown()
+    {
+        spriterender.sprite = firstSprite;
+       
+       
+
+    }
+
+}
